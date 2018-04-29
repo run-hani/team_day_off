ee1fbfc3e02592f394172bbc1647bd3b496be6b0
hani.run <myrnvl@gmail.com>
2018-04-29 22:28:55 +0900

    전년도 대체휴무 추가/사용 내역에 대한 계산 추가

--- src/components/Vacation/VacaList/Calc.vue
+++ src/components/Vacation/VacaList/Calc.vue
@@ -7,6 +7,7 @@
         </select>
 
         <select name="" id="" class="select wf80" v-model="printData.selecYear">
+          <option value="2017">2017</option>
           <option value="2018">2018</option>
         </select>
         <button class="btn t_m wf60 c_weight" @click="getVacation">확인</button>
@@ -16,36 +17,55 @@
     <p class="txt-t02" v-if="isPrint"><strong class="em-c04">{{ getData.userId }}</strong>의 <span class="em-c02">{{ getData.selecYear }}</span>년 대체휴가 사용내역입니다.</p>
     <p class="box-t01 t_info" v-if="!isPrint"><strong>{{ getData.selecYear }}</strong>년에 사용한 대체휴가가 없습니다.</p>
 
+    <div class="box-t01 t_info">
+      <h4 class="h-t04"><strong>휴가산정  방법</strong></h4>
+      <ul class="list-t01">
+        <li><strong>'16시간 = 1일'</strong> 인 경우 : 모든 프로젝트 주중 야근 시 계산되며 8시간 이상 시 반올림 처리 됨. <small class="em-c01">(ex. 10시간 근무 = 1일, 23시간 근무 = 1일, 29시간 근무 2일)</small></li>
+        <li><strong>'8시간 = 1일'</strong> 인 경우 : '온라인 스토어' 프로젝트 이외 모든 프로젝트 주말/휴일 출근 시.</li>
+      </ul>
+    </div>
+
     <table class="tbl-list01" v-if="isPrint">
       <colgroup>
-        <col style="width:25%">
-        <col style="width:25%">
-        <col style="width:25%">
-        <col style="width:25%">
+        <col style="width:20%">
+        <col style="width:20%">
+        <col style="width:20%">
+        <col style="width:20%">
+        <col style="width:20%">
       </colgroup>
       <thead>
       <tr>
         <th scope="col">월</th>
-        <th scope="col">발생</th>
+        <th scope="col">발생<br>(16시간 = 1일)</th>
+        <th scope="col">발생<br>(8시간 = 1일)</th>
+        <th scope="col">발생 합계</th>
         <th scope="col">사용</th>
       </tr>
       </thead>
       <tbody>
-      <!--<tr v-for="(user, key, idx) in listUseVacation">-->
-        <!--<td>{{ user.userId }}</td>-->
-        <!--<td>{{ user.useDate }}</td>-->
-      <!--</tr>-->
-      <tr v-for="idx in 12">
+      <tr>
+        <td>직전 총계</td>
+        <td><span class="em-slight">{{ countBeforeWorkTime }}</span></td>
+        <td><span class="em-slight">{{ countBeforeWorkTimeX2 }}</span></td>
+        <td><span>{{ countBeforeCalcWorkTime }}<br>
+          합계 : {{ countBeforeAllWorkTime }}</span></td>
+        <td>{{ countBeforeUseVacation }}<br>
+          합계 : {{ countBeforeAllUseVacation }}</td>
+      </tr>
+      <tr v-for="idx in 12" :class="{tr_active: idx == getTodayMonth()}">
         <td>{{ idx }}월</td>
-        <td><span>{{ countCalcWorkTime[idx-1] }}</span> <span v-if="countWorkTime[idx-1]" class="em-slight">( {{ countWorkTime[idx-1] }} )</span></td>
+        <td><span class="em-slight">{{ countWorkTime[idx-1] }}</span></td>
+        <td><span class="em-slight">{{ countWorkTimeX2[idx-1] }}</span></td>
+        <td><span>{{ countCalcWorkTime[idx-1] }}</span></td>
         <td>{{ countUseVacation[idx-1] }}</td>
       </tr>
       <tr>
         <td><strong>총계</strong></td>
-        <td colspan="2">
-          <div class="tag-t02"><strong>발생 : <span class="em-c04">{{ countAllWorkTime }}</span></strong> 일</div>
+        <td colspan="4">
+          <div class="tag-t02">( <strong>작년 : <span class="em-c04">{{ countBeforeAllWorkTime - countBeforeAllUseVacation }}</span></strong> 일</div> +
+          <div class="tag-t02"><strong>발생 : <span class="em-c04">{{ countAllWorkTime }}</span></strong> 일 )</div> -
           <div class="tag-t02"><strong>사용 : <span class="em-c03">{{ countAllUseVacation }}</span></strong> 일</div>
-          <div class="tag-t02"><strong>= 잔여 : <span class="em-c02">{{ countAllWorkTime - countAllUseVacation }}</span></strong> 일</div>
+          <div class="tag-t02"><strong>= 잔여 : <span class="em-c02">{{ countAllWorkTime - countAllUseVacation + (countBeforeAllWorkTime - countBeforeAllUseVacation) }}</span></strong> 일</div>
         </td>
       </tr>
       </tbody>
@@ -69,19 +89,30 @@
       return {
         listWorkTime: [],
         countWorkTime: [],
+        listWorkTimeX2: [],
+        countWorkTimeX2: [],
         countCalcWorkTime: [],
         countAllWorkTime: 0,
+
         listOccurVacation: [],
         listUseVacation: [],
         countUseVacation: [],
         countAllUseVacation: 0,
+
+        countBeforeWorkTime: {},
+        countBeforeWorkTimeX2: {},
+        countBeforeCalcWorkTime: {},
+        countBeforeAllWorkTime: 0,
+        countBeforeUseVacation: {},
+        countBeforeAllUseVacation: 0,
+
         getData: {
           userId: 'PD',
-          selecYear: '2018'
+          selecYear: this.getTodayYear()
         },
         printData: {
           userId: 'PD',
-          selecYear: '2018'
+          selecYear: this.getTodayYear()
         },
         isPrint: false,
         options: option
@@ -96,6 +127,8 @@
             const datas = []
             this.listUseVacation = []
             this.countUseVacation = [0,0,0,0,0,0,0,0,0,0,0,0]
+            this.countBeforeUseVacation = {}
+            this.countBeforeAllUseVacation = 0
 
             let selecUserId = this.printData.userId
             let selecYear = this.printData.selecYear
@@ -115,9 +148,18 @@
                 datas.push(tempData)
                 this.setMonthData(this.countUseVacation, data[key])
               }
+
+              // 직전년도 사용일 검색
+              if (selecUserId == data[key].userId && selecYear > selecYear_temp) {
+                let _temp = this.countBeforeUseVacation
+                if(_temp[selecYear_temp] == null)
+                  _temp[selecYear_temp] = 0
+                _temp[selecYear_temp] += 1
+                this.countBeforeAllUseVacation += 1
+              }
             }
 
-            /* 사용일 합산 */
+            /* 선택년도 사용일 합산 */
             this.countAllUseVacation = this.getCountAll(this.countUseVacation)
 
             if(datas.length) {
@@ -126,7 +168,8 @@
             }else{
               this.isPrint = false
             }
-            //alert(this.countUseVacation)
+
+            this.getWorks()
           })
           .catch(error => {
             console.log(error)
@@ -141,21 +184,33 @@
             const datas = []
             this.listWorkTime = []
             this.countWorkTime = [0,0,0,0,0,0,0,0,0,0,0,0]
+            this.countWorkTimeX2 = [0,0,0,0,0,0,0,0,0,0,0,0]
             this.countCalcWorkTime = [0,0,0,0,0,0,0,0,0,0,0,0]
+            this.countBeforeWorkTime = {}
+            this.countBeforeWorkTimeX2 = {}
+            this.countBeforeCalcWorkTime = {}
+            this.countBeforeAllWorkTime = 0
 
             let selecUserId = this.printData.userId
             let selecYear = this.printData.selecYear
+
+            let _temp = this.countBeforeWorkTime
+            let _temp2 = this.countBeforeWorkTimeX2
+            let _temp3 = this.countBeforeCalcWorkTime
+            let countBeforeAll = this.countBeforeAllWorkTime
 
             this.getData.userId = this.printData.userId
             this.getData.selecYear = this.printData.selecYear
 
             for (let key in data) {
-              let selecYear_temp = this.customFormatter2(data[key].useDate)
+              let selecYear_temp = this.customFormatter2(data[key].workDate)
+              let selecMonth_temp = this.customFormatter3(data[key].workDate)
 
-              if (selecUserId == data[key].userId && selecYear == selecYear_temp) {
+              if (selecUserId == data[key].userId && selecYear === selecYear_temp) {
                 const tempData = {
                   id: key,
                   userId: data[key].userId,
+                  pjType: data[key].pjType,
                   workHours: data[key].workHours,
                   workDate: this.customFormatter(data[key].workDate),
                   isWeekend: data[key].isWeekend
@@ -163,39 +218,130 @@
 
                 datas.push(tempData)
                 if (!tempData.isWeekend) {
-                  //alert(data[key].workHours)
-                  let arr = this.countWorkTime
+                  // 평일 : 모든 프로젝트 공통
                   switch(this.customFormatter3(data[key].workDate)) {
-                    case "01": this.countWorkTime[0] += Number(data[key].workHours); break
-                    case "02": this.countWorkTime[1] += Number(data[key].workHours); break
-                    case "03": this.countWorkTime[2] += Number(data[key].workHours); break
-                    case "04": this.countWorkTime[3] += Number(data[key].workHours); break
-                    case "05": this.countWorkTime[4] += Number(data[key].workHours); break
-                    case "06": this.countWorkTime[5] += Number(data[key].workHours); break
-                    case "07": this.countWorkTime[6] += Number(data[key].workHours); break
-                    case "08": this.countWorkTime[7] += Number(data[key].workHours); break
-                    case "09": this.countWorkTime[8] += Number(data[key].workHours); break
-                    case "10": this.countWorkTime[9] += Number(data[key].workHours); break
-                    case "11": this.countWorkTime[10] += Number(data[key].workHours); break
-                    case "12": this.countWorkTime[11] += Number(data[key].workHours); break
+                    case "1": this.countWorkTime[0] += data[key].workHours; break
+                    case "2": this.countWorkTime[1] += data[key].workHours; break
+                    case "3": this.countWorkTime[2] += data[key].workHours; break
+                    case "4": this.countWorkTime[3] += data[key].workHours; break
+                    case "5": this.countWorkTime[4] += data[key].workHours; break
+                    case "6": this.countWorkTime[5] += data[key].workHours; break
+                    case "7": this.countWorkTime[6] += data[key].workHours; break
+                    case "8": this.countWorkTime[7] += data[key].workHours; break
+                    case "9": this.countWorkTime[8] += data[key].workHours; break
+                    case "10": this.countWorkTime[9] += data[key].workHours; break
+                    case "11": this.countWorkTime[10] += data[key].workHours; break
+                    case "12": this.countWorkTime[11] += data[key].workHours; break
+                  }
+                } else if (tempData.isWeekend && tempData.pjType != 0) {
+                  // 주말 또는 휴일 : 온라인스토어는 제외
+                  switch(this.customFormatter3(data[key].workDate)) {
+                    case "1": this.countWorkTimeX2[0] += data[key].workHours; break
+                    case "2": this.countWorkTimeX2[1] += data[key].workHours; break
+                    case "3": this.countWorkTimeX2[2] += data[key].workHours; break
+                    case "4": this.countWorkTimeX2[3] += data[key].workHours; break
+                    case "5": this.countWorkTimeX2[4] += data[key].workHours; break
+                    case "6": this.countWorkTimeX2[5] += data[key].workHours; break
+                    case "7": this.countWorkTimeX2[6] += data[key].workHours; break
+                    case "8": this.countWorkTimeX2[7] += data[key].workHours; break
+                    case "9": this.countWorkTimeX2[8] += data[key].workHours; break
+                    case "10": this.countWorkTimeX2[9] += data[key].workHours; break
+                    case "11": this.countWorkTimeX2[10] += data[key].workHours; break
+                    case "12": this.countWorkTimeX2[11] += data[key].workHours; break
                   }
                 }
+              } else if (selecUserId == data[key].userId && selecYear > selecYear_temp) {
+                //alert("!")
+              }
+
+              // 직전년도 발생 합계
+              if (selecUserId == data[key].userId && selecYear > selecYear_temp) {
+                const tempData = {
+                  workHours: data[key].workHours,
+                  workYear: this.customFormatter2(data[key].workDate),
+                  workMonth: this.customFormatter3(data[key].workDate),
+                  isWeekend: data[key].isWeekend
+               }
+
+                if(_temp[selecYear_temp] == null)
+                  _temp[selecYear_temp] = new Array()
+
+                if(_temp2[selecYear_temp] == null)
+                  _temp2[selecYear_temp] = new Array()
+
+                // 평일
+                if (!tempData.isWeekend) {
+                  if(_temp[selecYear_temp][selecMonth_temp] == null)
+                    _temp[selecYear_temp][selecMonth_temp] = 0
+
+                  _temp[selecYear_temp][selecMonth_temp] += tempData.workHours
+                  console.log(selecYear_temp + ":" +_temp[selecYear_temp][selecMonth_temp])
+
+                // 주말 또는 휴일 : 온라인스토어는 제외
+                } else if (tempData.isWeekend && tempData.pjType != 0) {
+
+                  if(_temp2[selecYear_temp][selecMonth_temp] == null)
+                    _temp2[selecYear_temp][selecMonth_temp] = 0
+
+                  _temp2[selecYear_temp][selecMonth_temp] += (tempData.workHours * 2)
+                }
+
+                _temp3[selecYear_temp] = new Array()
+                console.log(_temp)
+              }
+            }
+
+            var temp4 = Object.keys(_temp3)
+            //console.log(Object.keys(_temp3).length)
+
+            for(let i = 0; i < Object.keys(_temp3).length; i++){
+              let curYear = temp4[i]
+              //console.log(curYear)
+
+              for(let j = 0; j < 13; j++) {
+                if(_temp[curYear][j] == null)
+                  _temp[curYear][j] = 0
+
+                if(_temp2[curYear][j] == null)
+                  _temp2[curYear][j] = 0
+
+                //alert(_temp[curYear][j])
+                _temp3[curYear][j] = _temp[curYear][j] + _temp2[curYear][j]
+              }
+            }
+
+            //직전년도 계산
+            for(let i = 0; i < Object.keys(_temp3).length; i++){
+              let curYear = temp4[i]
+              for(let j = 0; j < 13; j++) {
+
+                this.countBeforeAllWorkTime += parseInt(_temp3[curYear][j] / 16)
+
+                if((_temp3[curYear][j] % 16) >= 8)
+                {
+                  this.countBeforeAllWorkTime += 1
+                }
+
               }
             }
 
             for (let i = 0; i < this.countWorkTime.length; i++) {
-              this.countCalcWorkTime[i] = parseInt(this.countWorkTime[i] / 16)
+              let temp_sum = this.countWorkTime[i] + (this.countWorkTimeX2[i] * 2)
 
-              if(parseInt(this.countWorkTime[i] % 16) >= 8)
+              this.countCalcWorkTime[i] = parseInt(temp_sum / 16)
+
+              if(parseInt(temp_sum % 16) >= 8)
               {
                 this.countCalcWorkTime[i] += 1
               }
             }
 
+            //alert(this.countCalcWorkTime)
+
             /* 발생일 합산 */
             this.countAllWorkTime = this.getCountAll(this.countCalcWorkTime)
 
-            //alert(this.printHoursWeekday)
+            //alert(this.countAllWorkTime)
 
             if(datas.length) {
               this.listWorkTime = datas
@@ -217,17 +363,17 @@
         }
         return count
       },
-      setMonthData(arr, dataKey) {
+      setMonthData (arr, dataKey) {
         switch(this.customFormatter3(dataKey.useDate)) {
-          case "01": arr[0] += 1; break
-          case "02": arr[1] += 1; break
-          case "03": arr[2] += 1; break
-          case "04": arr[3] += 1; break
-          case "05": arr[4] += 1; break
-          case "06": arr[5] += 1; break
-          case "07": arr[6] += 1; break
-          case "08": arr[7] += 1; break
-          case "09": arr[8] += 1; break
+          case "1": arr[0] += 1; break
+          case "2": arr[1] += 1; break
+          case "3": arr[2] += 1; break
+          case "4": arr[3] += 1; break
+          case "5": arr[4] += 1; break
+          case "6": arr[5] += 1; break
+          case "7": arr[6] += 1; break
+          case "8": arr[7] += 1; break
+          case "9": arr[8] += 1; break
           case "10": arr[9] += 1; break
           case "11": arr[10] += 1; break
           case "12": arr[11] += 1; break
@@ -240,7 +386,15 @@
         return moment(date).format('YYYY');
       },
       customFormatter3(date) {
-        return moment(date).format('MM');
+        return moment(date).format('M');
+      },
+      getTodayYear () {
+        let newDate = new Date();
+        return moment(newDate).format('YYYY');
+      },
+      getTodayMonth () {
+        let newDate = new Date();
+        return moment(newDate).format('M');
       }
     },
     created () {
