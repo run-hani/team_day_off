a5b0cd55800d19fd255e4d6eb52d37273a920c64
hani.run <myrnvl@gmail.com>
2018-05-04 10:34:41 +0900

    [180504] Home 추가

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="contents" class="contents">
+    냠냠냠냠냠
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: 'Home',
+    created () {
+      this.$store.dispatch('fetchUser')
+    }
+  }
+</script>
+
+<style scoped>
+  h1, p {
+    text-align: center;
+  }
+
+  p {
+    color: red;
+  }
+</style>
